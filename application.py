d90e422116897d05531cb233f850a4fc774aefad
Minh Dinh <minhy.dinhy@outlook.com>
2023-07-21 20:31:35 -0500

    added database connection

--- application.py
+++ application.py
@@ -1,7 +1,19 @@
-from flask import Flask
-from flask import render_template;
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 application = Flask(__name__)
+
+application.config['SQLALCHEMY_DATABASE_URI']='mysql://root:password@awseb-e-f39h2b6uxw-stack-awsebrdsdatabase-oufcg7eqqkbx.cww0u0pb9txq.us-east-2.rds.amazonaws.com/user'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+application.secret_key = "cedarhilltailor"
+
+db = SQLAlchemy(application)
+
+class User(db.Model):
+    username = db.Column(db.String(120), primary_key=True)
+    password = db.Column(db.String(120), nullable=False)
+
+
 
 @application.route('/')
 def home():
