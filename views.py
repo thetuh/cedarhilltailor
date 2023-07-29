b607689c83077fdf02a94e6d8d48885bd68fefd9
Minh Dinh <Minh.Dinh-1@ou.edu>
2023-07-28 23:28:37 -0500

    added working user editing functionality within 'manage users' feature

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_required, current_user
 
 from models import User
@@ -7,30 +8,69 @@ views = Blueprint('views', __name__)
 
 # [ GUEST ] ---
 
-@views.route('/', methods=['GET', 'POST'])
+@views.route('/')
 def home():
     return render_template('home.html')
 
-@views.route('/search-order', methods=['GET', 'POST'])
+@views.route('/search-order')
 def search_order():
     return render_template('search-order.html')
 
 # [ MANAGER ] ---
 
-@views.route('/create-order', methods=['GET', 'POST'])
+@views.route('/create-order')
 @login_required
 def create_order():
     return render_template('create-order.html')
 
 # [ ADMIN ] ---
 
-@views.route('/users', methods=['GET', 'POST'])
+@views.route('/users')
 @login_required
 def manage_users():
     if current_user.id == 1:
         # Query all users except the admin user to prevent accidental deletion
         users = User.query.filter(User.id != 1).all()
         return render_template('users.html', users=users)
+    else:
+        flash('Unauthorized access', category='error')
+        return redirect(url_for('views.home'))
+
+@views.route('/users/edit-user', methods=['GET', 'POST'])
+@login_required
+def edit_user():
+    if current_user.id == 1:
+        if request.method == 'POST':
+            user = User.query.get(request.form.get('id'))
+            if not user:
+                flash('User not found', category='error')
+                return redirect(url_for('views.manage_users'))
+
+            username = request.form.get('username')
+            password = request.form.get('password')
+
+            error = []
+
+            # Sanity checks
+            if len(username) < 1:
+                error.append('Please enter valid username')
+            elif len(password) < 1:
+                error.append('Please enter valid password')
+            elif check_password_hash(user.password, password):
+                error.append('Password is already in use')
+
+            # Error message display
+            if error:
+                error_message = ' '.join(error)
+                flash(error_message, category='error')
+                return redirect(url_for('views.manage_users'))
+
+            new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
+            db.session.add(User)
+            db.session.commit()
+
+            flash('Successfully updated ' + user.username, category='success')
+            return redirect(url_for('views.manage_users'))
     else:
         flash('Unauthorized access', category='error')
         return redirect(url_for('views.home'))
